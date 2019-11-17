7b77665f33eb0b3cced02b99710b447b8014edb0
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2019-11-17 09:07:30 -0700

    Remove unneeded secret expansion. This is now handled better.

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,20 +52,4 @@ if cat /run/secrets/admin_password | grep "Not yet set." &> /dev/null; then
     sleep 1000d
 fi
 
-# https://unix.stackexchange.com/questions/79068/how-to-export-variables-that-are-set-all-at-once
-set -a
-    # TODO: Rather than only set these users when the DB is initialized, they should be idempotently set. 
-    INFLUXDB_ADMIN_USER=$(cat /run/secrets/admin_username)
-    INFLUXDB_ADMIN_PASSWORD=$(cat /run/secrets/admin_password)
-
-    # Could consider using WRITE_USER instead.
-    INFLUXDB_USER=$(cat /run/secrets/home_assistant_username)
-    INFLUXDB_USER_PASSWORD=$(cat /run/secrets/home_assistant_password)
-    INFLUXDB_READ_USER=$(cat /run/secrets/grafana_username)
-    INFLUXDB_READ_USER_PASSWORD=$(cat /run/secrets/grafana_password)
-set +a
-
-# Launch Influx
 /entrypoint.sh influxd
-
-exec "$@"
